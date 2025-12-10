c4eead2c72b38fb165eb813e022a74a642857238
Brian Igbinovia <320410@student.nottinghamcollege.ac.uk>
2025-12-10 13:53:22 +0000

    files

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import sys
 import pygame
+import assets.variables as var
 #to run the code, use cd C:\Users\osabr\OneDrive\Documents\GitHub\cheeseTD3
 #.\.venv\Scripts\Activate.ps1
 #python main.py
 print(f'Running with {sys.executable}')
 
-# Initialize Pygame
+
+#set up clock
+clock = pygame.time.Clock()
+
+
+# Initialize Pygame and the game window
 pygame.init()
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((var.SCREEN_WIDTH, var.SCREEN_HEIGHT))
 pygame.display.set_caption("CheeseTD3")
+
+
+#Player rectangle
+player = pygame.Rect((300, 250, 50, 50))
+
 
 # Game loop
 running = True
 while running:
+    clock.tick(var.FPS)
+
+   
+
+    #Exit game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
