41a504382e5a788a0cb2631306a9b89f5177abc1
Brian Igbinovia <320410@student.nottinghamcollege.ac.uk>
2026-03-06 21:57:35 +0000

    tuff colour change

--- dotpy files/variables.py
+++ dotpy files/variables.py
@@ -5,7 +5,7 @@ sidebar = 300
 SCREEN_WIDTH = TileSize * Column
 SCREEN_HEIGHT = TileSize * Row
 FPS = 60
-BACKGROUND_COLOR = (240, 230, 140)  # light goldenrod (RGB)
+BACKGROUND_COLOR = (60, 60, 60)  
 GREY0 = (60, 60, 60)
 #myce stats
 #myce animations
